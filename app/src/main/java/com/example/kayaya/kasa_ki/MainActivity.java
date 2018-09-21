26f95ae5922b3053a63c230b380e7015d7d81323
Kayaya <tiagokayaya@hotmail.com>
2018-09-21 23:06:42 +0100

    Added a navigation bar with a menu with a link and settings. Added 2 icons Added some strings Added some colors

--- app/src/main/java/com/example/kayaya/kasa_ki/MainActivity.java
+++ app/src/main/java/com/example/kayaya/kasa_ki/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.kayaya.kasa_ki;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+    //Setting up the Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+
+                return true;
+
+            case R.id.action_upload:
+
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
